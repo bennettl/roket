58e870a741fdd08a181891438a8b014f4ccafe84
Zack Tanner <zacktanner@gmail.com>
2015-01-27 16:00:28 -0800

    profile image model method

--- krave/api/models.py
+++ krave/api/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialAccount
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def profile_image_url(self):
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+        return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
 
 
 class Category(models.Model):
