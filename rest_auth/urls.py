ddea19dbc3b3cf81c940f5c2ceb445d0c928eb7e
Zack Tanner <zacktanner@gmail.com>
2015-01-27 22:51:30 -0800

    undo

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -5,22 +5,14 @@ from rest_auth.views import (Login, Logout, UserDetails, PasswordChange,
 
 urlpatterns = patterns('',
                        # URLs that do not require a session or valid token
-                       # url(r'^password/reset/$', PasswordReset.as_view(),
-                       #     name='rest_password_reset'),
-                       # url(r'^password/reset/confirm/$', PasswordResetConfirm.as_view(),
-                       #     name='rest_password_reset_confirm'),
+                       url(r'^password/reset/$', PasswordReset.as_view(),
+                           name='rest_password_reset'),
+                       url(r'^password/reset/confirm/$', PasswordResetConfirm.as_view(),
+                           name='rest_password_reset_confirm'),
                        url(r'^login/$', Login.as_view(), name='rest_login'),
                        # URLs that require a user to be logged in with a valid session / token.
                        url(r'^logout/$', Logout.as_view(), name='rest_logout'),
                        url(r'^user/$', UserDetails.as_view(), name='rest_user_details'),
                        url(r'^password/change/$', PasswordChange.as_view(),
                            name='rest_password_change'),
-
-
-                       url(r'^password-reset/$',
-                           TemplateView.as_view(template_name="password_reset.html"),
-                           name='password-reset'),
-                       url(r'^password-reset/confirm/$',
-                           TemplateView.as_view(template_name="password_reset_confirm.html"),
-                           name='password-reset-confirm'),
                        )
