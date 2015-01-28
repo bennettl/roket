1e00a73ad78c3406ef845843d7c730617cc690b0
Zack Tanner <zacktanner@gmail.com>
2015-01-27 16:50:33 -0800

    serializer work;

--- krave/api/serializers.py
+++ krave/api/serializers.py
@@ -7,7 +7,7 @@ from .models import Post, Category, Votes, Comments, Replies
 
 
 class UserSerializer(serializers.ModelSerializer):
-    profile_image = serializers.SerializerMethodField('get_profile_image')
+    profile_image = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'profile_image' )
