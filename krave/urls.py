10270b2df7edb727b1fe2b9a8e947efcdecca98e
Zack Tanner <zacktanner@gmail.com>
2015-01-27 23:03:46 -0800

    blah

--- krave/urls.py
+++ krave/urls.py
@@ -39,17 +39,14 @@ urlpatterns = patterns('',
     url(r'^editProfile', 'krave.views.public', name='public'),
     url(r'^profile', 'krave.views.public', name='public'),
     url(r'^about', 'krave.views.public', name='public'),
+    url(r'^passwordReset', 'krave.views.public', name='public'),
 
-    url(r'^password-reset/$',
-        TemplateView.as_view(template_name="password_reset.html"),
-        name='password-reset'),
-    url(r'^password-reset/confirm/$',
-        TemplateView.as_view(template_name="password_reset_confirm.html"),
-        name='password-reset-confirm'),
+
 
     # this url is used to generate email content
     url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         TemplateView.as_view(template_name="password_reset_confirm.html"),
         name='password_reset_confirm'),
+
 
 )
