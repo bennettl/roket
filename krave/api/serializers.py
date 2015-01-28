1b1007b609598728c5c42737254c3e3df33b224f
Zack Tanner <zacktanner@gmail.com>
2015-01-27 16:04:36 -0800

    added user profile to serializer

--- krave/api/serializers.py
+++ krave/api/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', )
+        fields = ('id', 'username', 'first_name', 'last_name', 'profile_image_url', )
 
 
 class CategorySerializer(serializers.ModelSerializer):
