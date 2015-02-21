a27af508a170f2228c192a3e4d45f4833e6cc40c
Zack Tanner <zacktanner@gmail.com>
2015-02-21 15:54:58 -0800

    allowed_hosts for production

--- krave/settings.py
+++ krave/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['54.191.169.225']
+ALLOWED_HOSTS = ['54.191.169.225', 'roket.tv']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
