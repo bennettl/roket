f36d24cea81b1884eaef0390e5b9f20b574b3bf4
Zack Tanner <zacktanner@gmail.com>
2015-02-21 12:17:43 -0800

    page to 1 week

--- krave/settings.py
+++ krave/settings.py
@@ -21,6 +21,18 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'roket',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'roketadmin',
+        'PASSWORD': 'denox111',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['54.191.169.225']
