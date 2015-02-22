da80d55dcfbf22c04ab0fbcef55e8cae38b130a3
Zack Tanner <zacktanner@gmail.com>
2015-02-21 16:26:46 -0800

    fixed url encoding problem

--- krave/assets/js/script.js
+++ krave/assets/js/script.js
@@ -187,7 +187,7 @@
 
                     if (url.indexOf('youtube.com') > -1 || url.indexOf('vimeo.com') > -1) {
 
-                        PostFactory.getUrlData(encodeURIComponent(original_url)).then(function (data) {
+                        PostFactory.getUrlData(original_url).then(function (data) {
                             $scope.newPost.title = data.title;
                         });
                         return true;
